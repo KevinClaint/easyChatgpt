3b31c41917abb631f083a38f256b049033705b46
KevinClaint <100783202+KevinClaint@users.noreply.github.com>
2023-07-27 10:59:12 +0800

    1
    1

---
+++ app.py
@@ -0,0 +1,30 @@
+import openai
+from flask import Flask, request, jsonify, render_template, Response
+import requests
+import json
+
+app = Flask(__name__)
+
+openai.api_key="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"
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/get_response", methods=["POST"])
+def get_response():
+    messages = request.form.get("user_input", None)
+    if messages is None:
+        return jsonify({"error": {"message": "请输入prompts！", "type": "invalid_request_error", "code": ""}})
+
+    # json串转对象
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": messages}]
+    )
+    reply=completion['choices'][0]['message']['content'].strip()
+
+    return render_template('index.html', user_input=messages, reply=reply)
+
+if __name__ == '__main__':
+    app.run(port=5000)
